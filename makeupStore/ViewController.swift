65ef064fb4da15617560b473b12db947e7d91e4a
Mila Alfitri <alfitriv@gmail.com>
2020-04-23 18:21:29 +0700

    Initial Commit

---
+++ makeupStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  makeupStore
+//
+//  Created by Vania Radmila Alfitri on 23/04/20.
+//  Copyright © 2020 Vania Radmila Alfitri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
